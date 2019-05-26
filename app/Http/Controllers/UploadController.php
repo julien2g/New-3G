7efa4b2d5b4639c36e5a29844b2cb38ef5043cf1
juliengadea <julien.gadea@yahoo.fr>
2019-05-26 12:50:33 +0800

    upload albums

---
+++ app/Http/Controllers/UploadController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UploadRequest;
+use App\ImageAlbum;
+use Illuminate\Http\Request;
+
+
+class UploadController extends Controller
+{
+    public function uplaod(UploadRequest $request)
+    {
+
+        $params = $request->except(['_token']);
+
+
+
+
+
+        /*
+                print_r($params);
+
+                echo '<br>';
+                echo '<br>';
+                echo '<br>';
+                echo '<br>';
+
+                echo $params['id'];*/
+
+
+        $slug = $this->generateRandomString(5);
+
+        $request->file('image')->storeAs('/public/' . $params['folder'], $slug . '.' . $request->file('image')->getClientOriginalExtension());
+
+
+        //echo 'File Extension: '.$request->file('image')->getClientOriginalExtension();
+        echo '<br>';
+
+
+        if ($params['folder'] == 'albums') {
+            $image_album = new ImageAlbum();
+
+            $image_album->id_album = $params['id'];
+            $image_album->slug = $slug;
+            $image_album->pos = $params['id'];
+            $image_album->ext = '.' . $request->file('image')->getClientOriginalExtension();
+
+
+            $image_album->save();
+
+        }elseif ($params['folder'] == 'chien')
+        {
+
+        }
+
+        return $image_album;
+        /*if (request()->hasFile('images')) {
+            $path = $request->file('images')->store('avatars');
+
+            echo $path;
+            die;
+        }
+        else{
+            echo'no';
+        }
+        /*$file = $request->file('image');
+
+        //Display File Name
+        echo 'File Name: '.$file->getClientOriginalName();
+        echo '<br>';
+
+        //Display File Extension
+        echo 'File Extension: '.$file->getClientOriginalExtension();
+        echo '<br>';
+
+        //Display File Real Path
+        echo 'File Real Path: '.$file->getRealPath();
+        echo '<br>';
+
+        //Display File Size
+        echo 'File Size: '.$file->getSize();
+        echo '<br>';
+
+        //Display File Mime Type
+        echo 'File Mime Type: '.$file->getMimeType();
+
+        //Move Uploaded File
+        $destinationPath = 'uploads';
+        $file->move($destinationPath,$file->getClientOriginalName());
+*/
+
+    }
+
+
+
+    public function generateRandomString($length) {
+        $characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+}

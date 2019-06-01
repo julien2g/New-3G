a2e04f6e554ab7e72070cbddc66dfac9d6e6de65
juliengadea <julien.gadea@yahoo.fr>
2019-06-01 14:50:29 +0800

    pro

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind('path.public', function() {
-            return base_path() . '/public_http';
+        $this->app->bind('path.public', function () {
+            return realpath(base_path() . '/../public_html');
         });
     }
 }
